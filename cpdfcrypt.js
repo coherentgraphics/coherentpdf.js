2b92fc3147b9b738b3eaa4d7c2513eabfdac24a4
John Whitington <john@coherentgraphics.co.uk>
2022-04-15 15:50:13 +0100

    more

--- cpdfcrypt.js
+++ cpdfcrypt.js
@@ -1,4 +1,41 @@
-function aes_cook_decrypt_key(s)
+//Provides: camlpdf_caml_aes_cook_encrypt_key 
+function camlpdf_caml_aes_cook_encrypt_key(s)
+{
+  return s;
+}
+
+//Provides: camlpdf_caml_aes_cook_decrypt_key 
+function camlpdf_caml_aes_cook_decrypt_key(s)
+{
+  return s;
+}
+
+//Provides: camlpdf_caml_aes_encrypt
+function camlpdf_caml_aes_encrypt(s, bs, i, bs2, i2)
+{
+  
+}
+
+//Provides: camlpdf_caml_aes_decrypt
+function camlpdf_caml_aes_decrypt(s, bs, i, bs2, i2)
+{
+  
+}
+
+//Provides: camlpdf_caml_sha256
+function camlpdf_caml_sha256(s)
+{
+  return s;
+}
+
+//Provides: camlpdf_caml_sha384
+function camlpdf_caml_sha384(s)
+{
+  return s;
+}
+
+//Provides: camlpdf_caml_sha512
+function camlpdf_caml_sha512(s)
 {
   return s;
 }
--- cpdfzlib.js
+++ cpdfzlib.js
@@ -1,4 +1,2 @@
-function caml_foo()
-{
-  return 42;
-}
+//Here we will actually need to modify pdfflate.ml or the ML module which
+//uses it to do the compression/decompression in the large.
