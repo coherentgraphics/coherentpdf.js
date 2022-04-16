d625bb7079f0737b351d195d045c0325db712796
John Whitington <john@coherentgraphics.co.uk>
2022-04-16 15:55:35 +0100

    more

--- cpdfcrypt.js
+++ cpdfcrypt.js
@@ -1,41 +1,48 @@
 //Provides: camlpdf_caml_aes_cook_encrypt_key 
 function camlpdf_caml_aes_cook_encrypt_key(s)
 {
+  console.log('camlpdf_caml_aes_cook_decrypt_key');
   return s;
 }
 
+//IMPL
 //Provides: camlpdf_caml_aes_cook_decrypt_key 
 function camlpdf_caml_aes_cook_decrypt_key(s)
 {
+  console.log('camlpdf_caml_aes_cook_decrypt_key');
   return s;
 }
 
 //Provides: camlpdf_caml_aes_encrypt
 function camlpdf_caml_aes_encrypt(s, bs, i, bs2, i2)
 {
-  
+  console.log('camlpdf_caml_aes_encrypt');
 }
 
+//IMPL
 //Provides: camlpdf_caml_aes_decrypt
 function camlpdf_caml_aes_decrypt(s, bs, i, bs2, i2)
 {
-  
+  console.log('camlpdf_caml_aes_decrypt');
 }
 
 //Provides: camlpdf_caml_sha256
 function camlpdf_caml_sha256(s)
-{
+{ 
+  console.log('camlpdf_caml_sha256');
   return s;
 }
 
 //Provides: camlpdf_caml_sha384
 function camlpdf_caml_sha384(s)
 {
+  console.log('camlpdf_caml_sha384');
   return s;
 }
 
 //Provides: camlpdf_caml_sha512
 function camlpdf_caml_sha512(s)
 {
+  console.log('camlpdf_caml_sha512');
   return s;
 }
