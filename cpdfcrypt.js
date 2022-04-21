9d4af85b3ea4b9da264b3125d14ae038778d151f
John Whitington <john@coherentgraphics.co.uk>
2022-04-21 16:35:28 +0100

    more

--- cpdfcrypt.js
+++ cpdfcrypt.js
@@ -5,15 +5,18 @@ function camlpdf_caml_aes_cook_encrypt_key(s)
   return s;
 }
 
-//This has to do rijndaelKeySetupDec(cooked_key, key, 8 * length(key))
+//The aes cipher. We set it up in camlpdf_caml_aes_cook_decrypt_key, but store
+//it here. The "cooked key" handed back to OCaml is junk and will be ignored
+//when sent to camlpdf_caml_aes_decrypt.
+var cpdf_aes;
 
 //Provides: camlpdf_caml_aes_cook_decrypt_key
 function camlpdf_caml_aes_cook_decrypt_key(s)
 {
   var s2 = caml_array_of_bytes(s);
-  //var key = sjcl.codec.bytes.toBits(s2);
+  var key = sjcl.codec.bytes.toBits(s2);
+  cpdf_aes = new sjcl.cipher.aes(key);
   var ba2 = Uint8Array.from(s2);
-  ba2.reverse();
   return caml_bytes_of_array(ba2);
 }
 
@@ -23,11 +26,11 @@ function camlpdf_caml_aes_encrypt(s, bs, i, bs2, i2)
   console.log('camlpdf_caml_aes_encrypt');
 }
 
-//This has to do rijndaelDecrypt(ckey, ???, src, dst)
-
 //Provides: camlpdf_caml_aes_decrypt
 function camlpdf_caml_aes_decrypt(ckey, src, src_ofs, dst, dst_ofs)
 {
+  console.log(cpdf_aes);
+  console.log(src, src_ofs, dst, dst_ofs);
   console.log('camlpdf_caml_aes_decrypt');
 }
 
