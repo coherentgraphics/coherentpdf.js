3f910ca23de8b73d915bd97075e79c845f7b8ee4
John Whitington <john@coherentgraphics.co.uk>
2022-04-24 15:30:40 +0100

    more

--- cpdflib.js
+++
@@ -1,6 +0,0 @@
-//This wraps the things exported from exports.ml into a JavaScript library.
-//We wrap and re-export.
-function ptOfCm(x)
-{
-  return cpdflib.ptOfCm(x);
-}
---
+++ cpdflibwrapper.js
@@ -0,0 +1,285 @@
+//This wraps the things exported from exports.ml into a JavaScript library.
+//We wrap and re-export.
+function setFast()
+{
+  return sjcl.cpdflib.setFast();
+}
+
+function setSlow()
+{
+  return sjcl.cpdflib.setSlow();
+}
+
+function version()
+{
+  return sjcl.cpdflib.version;
+}
+
+function ptOfCm(x)
+{
+  return sjcl.cpdflib.ptOfCm(x);
+}
+
+joo_global_object.cpdfjs =
+  {
+  //CHAPTER 1. Basics
+  setFast : setFast,
+  setSlow : setSlow,
+  version : version,
+  /*startEnumeratePDFs : startEnumeratePDFs,
+  enumeratePDFsKey : enumeratePDFsKey,
+  endEnumeratePDFs : endEnumeratePDFs,
+  deletePdf : deletePdf,
+  parsePagespec : parsePagespec,
+  stringOfPagespec : stringOfPagespec,*/
+  ptOfCm : ptOfCm
+  /*,
+  ptOfMm : ptOfMm,
+  ptOfIn : ptOfIn,
+  cmOfPt : cmOfPt,
+  mmOfPt : mmOfPt,
+  inOfPt : inOfPt,
+  range : range,
+  blankRange : blankrange,
+  makerange : makerange,
+  writerange : writerange,
+  readrange : readrange,
+  addtorange : addtorange,
+  even : even,
+  odd : odd,
+  union : union,
+  different : difference,
+  removeDuplicates : removeDuplicates,
+  isInRange : isInRange,
+  fromFile : fromFile,
+  fromFileLazy : fromFileLazy,
+  fromMemory : fromMemory,
+  fromMemoryLazy : fromMemoryLazy,
+  toFile : toFile,
+  toFileMemory : toFileMemory,
+  pages : pages,
+  pagesFast : pagesFast,
+  all : all,
+  isEncrypted : isEncrypted,
+  decryptPdf : decryptPdf,
+  decryptPdfOwner : decryptPdfOwner,
+  hasPermission : hasPermission,
+  encryptionKind : encryptionKind,
+
+  //CHAPTER 2. Merging and Splitting
+  mergeSimple : mergeSimple,
+  merge : merge,
+  mergeSame : mergeSame,
+  selectPages : selectPages,
+
+  //CHAPTER 3. Pages
+  scalePages : scalePages,
+  scaleToFit : scaleToFit,
+  scaleToFitPaper : scaleToFit,
+  scaleContents : scaleContents,
+  rotate : rotate,
+  rotateBy : rotateBy,
+  rotateContents : rotateContents,
+  upright : upright,
+  hFlip : hFlip,
+  vFlip : vFlip,
+  crop : crop,
+  setMediabox : setMediabox,
+  setCropBox : setCropBox,
+  setTrimBox : setTrimBox,
+  setArtBox : setArtBox,
+  getMediaBox : getMediaBox,
+  getCropBox : getCropBox,
+  getArtBox : getArtBox,
+  getBleedBox : getBleedBox,
+  getTrimBox : getTrimBox,
+  removeCrop : removeCrop,
+  removeArt : removeArt,
+  removeTrim : removeTrim,
+  removeBleed : removeBleed,
+  hardBox : hardBox,
+  trimMarks : trimMarks,
+  showBoxes : showBoxes,
+
+  //CHAPTER 4. Encryption and Decryption
+
+  //CHAPTER 5. Compression
+  compress : compress,
+  decompress : decompress,
+
+  //CHAPTER 6. Bookmarks
+  startGetBookmarkInfo : startGetBookmarkInfo,
+  endGetBookmarkInfo : endGetBookmarkInfo,
+  numberBookmarks : numberBookmarks,
+  getBookmarkPage : getBookmarkPage,
+  getBookmarkLevel : getBookmarkLevel,
+  getBookmarkText : getBookmarkText,
+  getBookmarkOpenStatus : getBookmarkOpenStatus,
+  startSetBookmarkInfo : startSetBookmarkInfo,
+  endSetBookmarkInfo : endSetBookmarkInfo,
+  setBookmarkPage : setBookmarkPage,
+  setBookmarkLevel : setBookmarkLevel,
+  setBookmarkOpenStatus : setBookmarkOpenStatus,
+  getBookmarksJSON : getBookmarksJSON,
+  setBookmarksJSON : setBookmarksJSON,
+  tableOfContents : tableOfContents,
+
+  //CHAPTER 7. Presentations
+
+  //CHAPTER 8. Logos, Watermarks and Stamps
+  stampOn : stampOn,
+  stampUnder : stampUnder,
+  stampExtended : stampExtended,
+  combinePages : combinePages,
+  addText : addText,
+  removeText : removeText,
+  addContent : addContent,
+  stampAsXObject : stampAsXObject,
+
+  //CHAPTER 9. Multipage facilities
+  twoUp : twoUp,
+  twoUpStack : twoUpStack,
+  impose : impose,
+  padBefore : padBefore,
+  padAfter : padAfter,
+  padEvery : padEvery,
+  padMultiple : padMultiple,
+  padMultipleBefore : padMultipleBefore,
+
+  //CHAPTER 10. Annotations
+  annotationsJSON : annotationsJSON,
+
+  //CHAPTER 11. Document Information and Metadata
+  getVersion : getVersion,
+  isLinearized : isLinearized,
+  getTitle : getTitle,
+  getAuthor : getAuthor,
+  getSubject : getSubject,
+  getKeywords : getKeywords,
+  getCreator : getCreator,
+  getProducer : getProducer,
+  getCreationDate : getCreationDate,
+  getModificationDate : getModificationDate,
+  getTitleXMP : getTitleXMP,
+  getAuthorXMP : getAuthorXMP,
+  getSubjectXMP : getSubjectXMP,
+  getKeywordsXMP : getKeywordsXMP,
+  getCreatorXMP : getCreatorXMP,
+  getProducerXMP : getProducerXMP,
+  getCreationDateXMP : getCreationDateXMP,
+  getModificationDateXMP : getModificationDateXMP,
+  getDateComponents : getDateComponents,
+  dateStringOfComponents : dateStringOfComponents,
+  setTitle : setTitle,
+  setAuthor : setAuthor,
+  setSubject : setSubject,
+  setKeywords : setKeywords,
+  setCreator : setCreator,
+  setProducer : setProducer,
+  setCreationDate : setCreationDate,
+  setModificationDate : setModificationDate,
+  markTrapped : markTrapped,
+  markUntrapped : markUntrapped,
+  setTitleXMP : setTitleXMP,
+  setAuthorXMP : setAuthorXMP,
+  setSubjectXMP : setSubjectXMP,
+  setKeywordsXMP : setKeywordsXMP,
+  setCreatorXMP : setCreatorXMP,
+  setProducerXMP : setProducerXMP,
+  setCreationDateXMP : setCreationDateXMP,
+  setModificationDateXMP : setModificationDateXMP,
+  hasBox : hasBox,
+  getPageRotation : getPageRotation,
+  setPageLayout : setPageLayout,
+  setPageMode : setPageMode,
+  hideToolbar : hideToolbar,
+  hideMenubar : hideMenubar,
+  hideWindowUi : hideWindowUi,
+  fitWindow : fitWindow,
+  centerWindow : centerWindow,
+  displayDocTitle : displayDocTitle,
+  openAtPage : openAtPage,
+  setMetadataFromFile : setMetadataFromFile,
+  getMetadata : getMetadata,
+  removeMetadata : removeMetadata,
+  createMetadata : createMetadata,
+  setMetadataDate : setMetadataDate,
+  addPageLabels : addPageLabels,
+  removePageLabels : removePageLabels,
+  startGetPageLabels : startGetPageLabels,
+  getPageLabelStyle : getPageLabelStyle,
+  getPageLabelPrefix : getPageLabelPrefix,
+  getPageLabelOffset : getPageLabelOffset,
+  getPageLabelRange : getPageLabelRange,
+  endGetPageLabels : endGetPageLabels,
+  getPageLabelStringForPage : getPageLabelStringForPage,
+
+  //CHAPTER 12. File Attachments
+  attachFile : attachFile,
+  attachFileToPage : attachFileToPage,
+  removeAttachedFiles : removeAttachedFiles,
+  startGetAttachments : startGetAttachments,
+  endGetAttachments : endGetAttachments,
+  numberGetAttachments : numberGetAttachments,
+  getAttachmentName : getAttachmentName,
+  getAttachmentPage : getAttachmentPage,
+  getAttachmentData : getAttachmentData,
+
+  //CHAPTER 13. Images
+  startGetImageResolution : startGetImageResolution,
+  getImageResolutionPageNumber : getImageResolutionPageNumber,
+  getImageResolutionImageName : getImageResolutionImageName,
+  getImageResolutionXPixels : getImageResolutionXPixels,
+  getImageResolutionYPixels : getImageResolutionYPixels,
+  getImageResolutionXRes : getImageResolutionXRes,
+  getImageResolutionYRes : getImageResolutionYRes,
+  endGetImageResolution : endGetImageResolution,
+
+  //CHAPTER 14. Fonts
+  numberFonts : numberFonts,
+  getFontPage : getFontPage,
+  getFontName : getFontName,
+  getFontType : getFontType,
+  getFontEncoding : getFontEncoding,
+  startGetFontInfo : startGetFontInfo,
+  endGetFontInfo : endGetFontInfo,
+  copyFont : copyFont,
+  removeFonts : removeFonts,
+
+  //CHAPTER 15. PDF and JSON
+  outputJSON : outputJSON,
+  outputJSONMemory : outputJSONMemory,
+  fromJSON : fromJSON,
+  fromJSONMemory : fromJSONMemory,
+
+  //CHAPTER 16. Optional Content Groups
+  startGetOCGList : startGetOCGList,
+  ocgListEntry : ocgListEntry,
+  endGetOCGList : endGetOCGList,
+  ocgCoalesce : ocgCoalesce,
+  ocgRename : ocgRename,
+  ocgOrderAll : ocgOrderAll,
+
+  //CHAPTER 17. Creating New PDFs
+  blankDocument : blankDocument,
+  blankDocumentPaper : blankDocumentPaper,
+  textToPDF : textToPDF,
+  textToPDFPaper : textToPDFPaper,
+
+  //CHAPTER 18. Miscellaneous
+  draft : draft,
+  removeAllText : removeAllText,
+  blackText : blackText,
+  blackLines : blackLines,
+  blackFills : blackFills,
+  thinLines : thinLines,
+  copyId : copyId,
+  removeId : removeId,
+  setVersion : setVersion,
+  setFullVersion : setFullVersion,
+  removeDictEntry : removeDictEntry,
+  removeDictEntrySearch : removeDictEntrySearch,
+  replaceDictEntry : replaceDictEntry,
+  replaceDictEntrySearch : replaceDictEntrySearch,
+  getDictEntries : getDictEntries,
+  removeClipping : removeClipping*/};
