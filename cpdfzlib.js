185ac753fe18744560dea518dd9794d98d4bb4b3
John Whitington <john@coherentgraphics.co.uk>
2022-05-12 19:05:11 +0100

    more

--- cpdfzlib.js
+++ cpdfzlib.js
@@ -4,7 +4,7 @@
 function camlpdf_caml_zlib_decompress(s)
 {
   var s2 = caml_array_of_bytes(s);
-  var output = zlib.inflateSync(s2); //FIXME Error handling
+  var output = zlib.inflateSync(s2);
   return caml_bytes_of_array(output);
 }
 
@@ -14,7 +14,7 @@ function camlpdf_caml_zlib_decompress(s)
 function camlpdf_caml_zlib_compress(s)
 {
   var s2 = caml_array_of_bytes(s);
-  var output = zlib.deflateSync(s2); //FIXME Error handling
+  var output = zlib.deflateSync(s2);
   return caml_bytes_of_array(output);
 }
 
@@ -23,11 +23,13 @@ function camlpdf_camlzip_inflateInit(b)
 {
   console.log("Cpdf.js does not currently support inline images compressed with zlib. Contact the author if this is a problem.");
 }
+
 //Provides: camlpdf_camlzip_inflateEnd
 function camlpdf_camlzip_inflateEnd(a, b, c, d, e, f, g, h)
 {
   console.log("Cpdf.js does not currently support inline images compressed with zlib. Contact the author if this is a problem.");
 }
+
 //Provides: camlpdf_camlzip_inflate_bytecode
 function camlpdf_camlzip_inflate_bytecode(b)
 {
