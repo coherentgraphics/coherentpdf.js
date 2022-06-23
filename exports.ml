12100dfee55bd9daed90ae72a5236183218cac4b
John Whitington <john@coherentgraphics.co.uk>
2022-06-23 17:01:42 +0100

    more

--- exports.ml
+++ exports.ml
@@ -4,7 +4,13 @@ exception CPDFError of string
 
 (* Check and raise in case of an error. *)
 let checkerror r =
-  if Cpdf.getLastError () != 0 then raise (CPDFError (Cpdf.getLastErrorString ())) else r
+  if Cpdf.getLastError () != 0 then
+    begin
+      let str = Cpdf.getLastErrorString () in
+        Cpdf.clearError ();
+        raise (CPDFError str)
+    end
+  else r
 
 (* Convert Cpdflib range to Javascript array of page numbers *)
 let array_of_range r =
@@ -51,119 +57,139 @@ let _ =
        method cmOfPt x = checkerror (Cpdf.cmOfPt x)
        method mmOfPt x = checkerror (Cpdf.mmOfPt x)
        method inOfPt x = checkerror (Cpdf.inOfPt x)
-       method range = Cpdf.range
-       method blankRange = Cpdf.blankrange ()
-       method makerange = Cpdf.makerange
-       method writerange = Cpdf.writerange
-       method rangeAdd = Cpdf.addtorange
-       method even = Cpdf.even
-       method odd = Cpdf.odd
-       method rangeUnion = Cpdf.union
-       method rangeLength = Cpdf.lengthrange
-       method rangeGet = Cpdf.readrange
-       method difference = Cpdf.difference
-       method removeDuplicates = Cpdf.removeDuplicates
-       method isInRange = Cpdf.isInRange
+       method range f t = checkerror (Cpdf.range f t)
+       method blankRange = checkerror (Cpdf.blankrange ())
+       method rangeAdd r page = checkerror (Cpdf.addtorange r page)
+       method even x = checkerror (Cpdf.even x)
+       method odd x = checkerror (Cpdf.odd x)
+       method rangeUnion a b = checkerror (Cpdf.union a b)
+       method rangeLength r = checkerror (Cpdf.lengthrange r)
+       method rangeGet r n = checkerror (Cpdf.readrange r n)
+       method difference a b = checkerror (Cpdf.difference a b)
+       method removeDuplicates x = checkerror (Cpdf.removeDuplicates x)
+       method isInRange r page = checkerror (Cpdf.isInRange r page)
        method fromFile filename userpw =
-         Cpdf.fromFile (Js.to_string filename) (Js.to_string userpw)
+         checkerror (Cpdf.fromFile (Js.to_string filename) (Js.to_string userpw))
        method fromFileLazy filename userpw =
-         Cpdf.fromFileLazy (Js.to_string filename) (Js.to_string userpw)
+         checkerror (Cpdf.fromFileLazy (Js.to_string filename) (Js.to_string userpw))
        method fromMemory = Cpdf.fromMemory (* data *)
        method fromMemoryLazy = Cpdf.fromMemoryLazy (* data *)
        method toFile pdf filename linearize make_id =
-         Cpdf.toFile pdf (Js.to_string filename) linearize make_id
+         checkerror (Cpdf.toFile pdf (Js.to_string filename) linearize make_id)
        method toFileExt pdf filename linearize make_id preserve_objstm create_objstm compress_objstm =
-         Cpdf.toFileExt pdf (Js.to_string filename) linearize make_id preserve_objstm create_objstm compress_objstm
+         checkerror (Cpdf.toFileExt pdf (Js.to_string filename) linearize make_id preserve_objstm create_objstm compress_objstm)
        method toFileEncrypted pdf encryption_method permissions ownerpw userpw linearize makeid filename =
-         Cpdf.toFileEncrypted
+         checkerror (Cpdf.toFileEncrypted
            pdf encryption_method (Js.to_array permissions) (Js.to_string ownerpw) (Js.to_string userpw)
-           linearize makeid (Js.to_string filename)
+           linearize makeid (Js.to_string filename))
        method toFileEncryptedExt
          pdf encryption_method permissions ownerpw userpw linearize makeid preserve_objstm generate_objstm
          compress_objstm filename
        =
-         Cpdf.toFileEncryptedExt
+         checkerror (Cpdf.toFileEncryptedExt
            pdf encryption_method (Js.to_array permissions) (Js.to_string ownerpw) (Js.to_string userpw)
-           linearize makeid preserve_objstm generate_objstm compress_objstm (Js.to_string filename)
+           linearize makeid preserve_objstm generate_objstm compress_objstm (Js.to_string filename))
        method toMemory = Cpdf.toFileMemory (* data *)
        method toMemoryExt = Cpdf.toFileMemoryExt (* data *)
        method toMemoryEncrypted = Cpdf.toFileMemoryEncrypted (* data *)
        method toMemoryEncryptedExt = Cpdf.toFileMemoryEncryptedExt (* data *)
-       method pages = Cpdf.pages
+       method pages pdf = checkerror (Cpdf.pages pdf)
        method pagesFast password filename =
-         Cpdf.pagesFast (Js.to_string password) (Js.to_string filename)
+         checkerror (Cpdf.pagesFast (Js.to_string password) (Js.to_string filename))
        method pagesFastMemory = Cpdf.pagesFastMemory (* data *)
-       method all = Cpdf.all
-       method isEncrypted = Cpdf.isEncrypted
+       method all pdf = checkerror (Cpdf.all pdf)
+       method isEncrypted pdf = checkerror (Cpdf.isEncrypted pdf)
        method decryptPdf pdf userpw =
-         Cpdf.decryptPdf pdf (Js.to_string userpw)
+         checkerror (Cpdf.decryptPdf pdf (Js.to_string userpw))
        method decryptPdfOwner pdf ownerpw =
-         Cpdf.decryptPdfOwner pdf (Js.to_string ownerpw)
-       method hasPermission = Cpdf.hasPermission
-       method encryptionKind = Cpdf.encryptionKind
+         checkerror (Cpdf.decryptPdfOwner pdf (Js.to_string ownerpw))
+       method hasPermission pdf permission = checkerror (Cpdf.hasPermission pdf permission)
+       method encryptionKind pdf = checkerror (Cpdf.encryptionKind pdf)
 
        (* CHAPTER 2. Merging and Splitting *)
-       method mergeSimple pdfs = Cpdf.mergeSimple (Js.to_array pdfs)
+       method mergeSimple pdfs = checkerror (Cpdf.mergeSimple (Js.to_array pdfs))
        method merge pdfs retain_numbering remove_duplicate_fonts =
-         Cpdf.merge (Js.to_array pdfs) retain_numbering remove_duplicate_fonts
-       method mergeSame = Cpdf.mergeSame (* FIXME array of arrays *)
-       method selectPages = Cpdf.selectPages
+         checkerror (Cpdf.merge (Js.to_array pdfs) retain_numbering remove_duplicate_fonts)
+       method mergeSame pdfs retain_numbering remove_duplicate_fonts ranges =
+         checkerror (Cpdf.mergeSame pdfs retain_numbering remove_duplicate_fonts ranges) (* FIXME array of arrays *)
+       method selectPages pdf range = checkerror (Cpdf.selectPages pdf range)
 
        (* CHAPTER 3. Pages *)
-       method scalePages = Cpdf.scalePages
-       method scaleToFit = Cpdf.scaleToFit
-       method scaleToFitPaper = Cpdf.scaleToFitPaper
-       method scaleContents = Cpdf.scaleContents(* TODO *)
-       method shiftContents = Cpdf.shiftContents(* TODO *)
-       method rotate = Cpdf.rotate(* TODO *)
-       method rotateBy = Cpdf.rotateBy(* TODO *)
-       method rotateContents = Cpdf.rotateContents(* TODO *)
-       method upright = Cpdf.upright(* TODO *)
-       method hFlip = Cpdf.hFlip(* TODO *)
-       method vFlip = Cpdf.vFlip(* TODO *)
-       method crop = Cpdf.crop(* TODO *)
-       method setMediabox = Cpdf.setMediabox(* TODO *)
-       method setCropBox = Cpdf.setCropBox(* TODO *)
-       method setTrimBox = Cpdf.setTrimBox(* TODO *)
-       method setArtBox = Cpdf.setArtBox(* TODO *)
-       method setBleedBox = Cpdf.setBleedBox(* TODO *)
-       method getMediaBox = Cpdf.getMediaBox(* TODO *)
-       method getCropBox = Cpdf.getCropBox(* TODO *)
-       method getArtBox = Cpdf.getArtBox(* TODO *)
-       method getBleedBox = Cpdf.getBleedBox(* TODO *)
-       method getTrimBox = Cpdf.getTrimBox(* TODO *)
-       method removeCrop = Cpdf.removeCrop(* TODO *)
-       method removeArt = Cpdf.removeArt(* TODO *)
-       method removeTrim = Cpdf.removeTrim(* TODO *)
-       method removeBleed = Cpdf.removeBleed(* TODO *)
-       method hardBox = Cpdf.hardBox(* TODO *)
-       method trimMarks = Cpdf.trimMarks(* TODO *)
-       method showBoxes = Cpdf.showBoxes(* TODO *)
+       method scalePages pdf range sx sy = checkerror (Cpdf.scalePages pdf range sx sy)
+       method scaleToFit pdf range sx sy scale = checkerror (Cpdf.scaleToFit pdf range sx sy scale)
+       method scaleToFitPaper pdf range papersize s =
+         checkerror (Cpdf.scaleToFitPaper pdf range papersize s)
+       method scaleContents pdf range position scale =
+         checkerror (Cpdf.scaleContents pdf range position scale) (* position *)
+       method shiftContents pdf range dx dy =
+         checkerror (Cpdf.shiftContents pdf range dx dy)
+       method rotate pdf range rotation =
+         checkerror (Cpdf.rotate pdf range rotation)
+       method rotateBy pdf range rotation =
+         checkerror (Cpdf.rotateBy pdf range rotation)
+       method rotateContents pdf range angle =
+         checkerror (Cpdf.rotateContents pdf range angle)
+       method upright pdf =
+         checkerror (Cpdf.upright pdf)
+       method hFlip pdf =
+         checkerror (Cpdf.hFlip pdf)
+       method vFlip pdf =
+         checkerror (Cpdf.vFlip pdf)
+       method crop pdf range x y w h =
+         checkerror (Cpdf.crop pdf range x y w h)
+       method setMediabox pdf range minx maxx miny maxy =
+         checkerror (Cpdf.setMediabox pdf range minx maxx miny maxy)
+       method setCropBox pdf range minx maxx miny maxy =
+         checkerror (Cpdf.setCropBox pdf range minx maxx miny maxy)
+       method setTrimBox pdf range minx maxx miny maxy =
+         checkerror (Cpdf.setTrimBox pdf range minx maxx miny maxy)
+       method setArtBox pdf range minx maxx miny maxy =
+         checkerror (Cpdf.setArtBox pdf range minx maxx miny maxy)
+       method setBleedBox pdf range minx maxx miny maxy =
+         checkerror (Cpdf.setBleedBox pdf range minx maxx miny maxy)
+       method getMediaBox pdf pagenumber =
+         checkerror (Js.array (let (a, b, c, d) = Cpdf.getMediaBox pdf pagenumber in [|a; b; c; d|]))
+       method getCropBox pdf pagenumber =
+         checkerror (Js.array (let (a, b, c, d) = Cpdf.getCropBox pdf pagenumber in [|a; b; c; d|]))
+       method getArtBox pdf pagenumber =
+         checkerror (Js.array (let (a, b, c, d) = Cpdf.getArtBox pdf pagenumber in [|a; b; c; d|]))
+       method getBleedBox pdf pagenumber =
+         checkerror (Js.array (let (a, b, c, d) = Cpdf.getBleedBox pdf pagenumber in [|a; b; c; d|]))
+       method getTrimBox pdf pagenumber =
+         checkerror (Js.array (let (a, b, c, d) = Cpdf.getTrimBox pdf pagenumber in [|a; b; c; d|]))
+       method removeCrop pdf range = checkerror (Cpdf.removeCrop pdf range)
+       method removeArt pdf range = checkerror (Cpdf.removeArt pdf range)
+       method removeTrim pdf range = checkerror (Cpdf.removeTrim pdf range)
+       method removeBleed pdf range = checkerror (Cpdf.removeBleed pdf range)
+       method hardBox pdf range boxname = checkerror (Cpdf.hardBox pdf range (Js.to_string boxname))
+       method trimMarks pdf range = checkerror (Cpdf.trimMarks pdf range)
+       method showBoxes pdf range = checkerror (Cpdf.showBoxes pdf range)
 
        (* CHAPTER 4. Encryption and Decryption *)
 
        (* CHAPTER 5. Compression *)
-       method compress = Cpdf.compress(* TODO *)
-       method decompress = Cpdf.decompress(* TODO *)
-       method squeezeInMemory = Cpdf.squeezeInMemory(* TODO *)
+       method compress pdf = checkerror (Cpdf.compress pdf)
+       method decompress pdf = checkerror (Cpdf.decompress pdf)
+       method squeezeInMemory pdf = checkerror (Cpdf.squeezeInMemory pdf)
 
        (* CHAPTER 6. Bookmarks *)
-       method startGetBookmarkInfo = Cpdf.startGetBookmarkInfo(* TODO *)
-       method endGetBookmarkInfo = Cpdf.endGetBookmarkInfo ()(* TODO *)
-       method numberBookmarks = Cpdf.numberBookmarks ()(* TODO *)
-       method getBookmarkPage = Cpdf.getBookmarkPage(* TODO *)
-       method getBookmarkLevel = Cpdf.getBookmarkLevel(* TODO *)
-       method getBookmarkText = Cpdf.getBookmarkText(* TODO *)
-       method getBookmarkOpenStatus = Cpdf.getBookmarkOpenStatus(* TODO *)
-       method startSetBookmarkInfo = Cpdf.startSetBookmarkInfo(* TODO *)
-       method endSetBookmarkInfo = Cpdf.endSetBookmarkInfo(* TODO *)
-       method setBookmarkPage = Cpdf.setBookmarkPage(* TODO *)
-       method setBookmarkLevel = Cpdf.setBookmarkLevel(* TODO *)
-       method setBookmarkText = Cpdf.setBookmarkText(* TODO *)
-       method setBookmarkOpenStatus = Cpdf.setBookmarkOpenStatus(* TODO *)
-       method getBookmarksJSON = Cpdf.getBookmarksJSON(* TODO *)
-       method setBookmarksJSON = Cpdf.setBookmarksJSON(* TODO *)
-       method tableOfContents = Cpdf.tableOfContents(* TODO *)
+       method startGetBookmarkInfo pdf = checkerror (Cpdf.startGetBookmarkInfo pdf)
+       method endGetBookmarkInfo = checkerror (Cpdf.endGetBookmarkInfo ())
+       method numberBookmarks = checkerror (Cpdf.numberBookmarks ())
+       method getBookmarkPage pdf n = checkerror (Cpdf.getBookmarkPage pdf n)
+       method getBookmarkLevel n = checkerror (Cpdf.getBookmarkLevel n)
+       method getBookmarkText n = checkerror (Js.string (Cpdf.getBookmarkText n))
+       method getBookmarkOpenStatus n = checkerror (Cpdf.getBookmarkOpenStatus n)
+       method startSetBookmarkInfo n = checkerror (Cpdf.startSetBookmarkInfo n)
+       method endSetBookmarkInfo pdf = checkerror (Cpdf.endSetBookmarkInfo pdf)
+       method setBookmarkPage pdf n page = checkerror (Cpdf.setBookmarkPage pdf n page)
+       method setBookmarkLevel n level = checkerror (Cpdf.setBookmarkLevel n level)
+       method setBookmarkText n text = Cpdf.setBookmarkText n (Js.to_string text)
+       method setBookmarkOpenStatus n status = checkerror (Cpdf.setBookmarkOpenStatus n status)
+       method getBookmarksJSON = Cpdf.getBookmarksJSON (* data *)
+       method setBookmarksJSON = Cpdf.setBookmarksJSON (* data *)
+       method tableOfContents pdf font fontsize title bookmark =
+         checkerror (Cpdf.tableOfContents pdf font fontsize (Js.to_string title) bookmark)
 
        (* CHAPTER 7. Presentations *)
 
@@ -179,17 +205,17 @@ let _ =
        method textWidth = Cpdf.textWidth(* TODO *)
 
        (* CHAPTER 9. Multipage facilities *)
-       method twoUp = Cpdf.twoUp(* TODO *)
-       method twoUpStack = Cpdf.twoUpStack(* TODO *)
-       method impose = Cpdf.impose(* TODO *)
-       method padBefore = Cpdf.padBefore(* TODO *)
-       method padAfter = Cpdf.padAfter(* TODO *)
-       method padEvery = Cpdf.padEvery(* TODO *)
-       method padMultiple = Cpdf.padMultiple(* TODO *)
-       method padMultipleBefore = Cpdf.padMultipleBefore(* TODO *)
+       method twoUp = Cpdf.twoUp
+       method twoUpStack = Cpdf.twoUpStack
+       method impose = Cpdf.impose
+       method padBefore = Cpdf.padBefore
+       method padAfter = Cpdf.padAfter
+       method padEvery = Cpdf.padEvery
+       method padMultiple = Cpdf.padMultiple
+       method padMultipleBefore = Cpdf.padMultipleBefore
 
        (* CHAPTER 10. Annotations *)
-       method annotationsJSON = Cpdf.annotationsJSON(* TODO *)
+       method annotationsJSON = Cpdf.annotationsJSON (* data *)
 
        (* CHAPTER 11. Document Information and Metadata *)
        method getVersion = Cpdf.getVersion(* TODO *)
